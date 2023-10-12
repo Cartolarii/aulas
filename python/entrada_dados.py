47e093dff4b18aa3e8ab7a922bd91a1b3b973f23
Cartolarii <147657799+Cartolarii@users.noreply.github.com>
2023-10-11 21:56:44 -0300

    Add files via upload

---
+++ python/entrada_dados.py
@@ -0,0 +1,9 @@
+a = input("digite o nome: ")
+print(a)
+
+
+a = int(input("digite um numero: "))
+b = int(input("digite um numero: "))
+c = int(input("digite um numero: "))
+soma = (a+b+c)/3
+print(soma)
